0f6b8b60783c7cb00e08057a7ee4d01ef56ce353
DEVKKJ1992 <kj.mahmood@sightbox.me>
2025-03-06 02:20:32 +0500

    Press-News Added

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import {siteSettings} from './siteSettings'
 import {postType} from './postType'
 import {whitepaperType} from './whitepaperType'
+import {newsType} from './newsType'
 
-export const schemaTypes = [siteSettings, postType, whitepaperType]
+export const schemaTypes = [siteSettings, postType, whitepaperType, newsType]
---
+++ schemaTypes/newsType.ts
@@ -0,0 +1,37 @@
+import {defineField, defineType} from 'sanity'
+
+export const newsType = defineType({
+  name: 'news',
+  title: 'Press & News',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'cta',
+      title: 'CTA URL',
+      type: 'url',
+    }),
+  ],
+})
